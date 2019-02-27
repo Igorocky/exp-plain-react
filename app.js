2156e53e9b805be83b4fea138302149d204b8c7d
Igorocky <Igorocky@users.noreply.github.com>
2019-02-27 03:35:57 +0100

    Add state into ListItem.

--- app.js
+++ app.js
@@ -2,17 +2,39 @@
 
 const re = React.createElement;
 
-const ListItem = props=>re('li',{},props.text)
+class ListItem extends React.Component {
+    constructor(props) {
+        console.log("ListItem.constructor. props = " + JSON.stringify(props))
+        super(props)
+        this.state={
+            likes: 0
+        }
+    }
+
+    render() {
+        return re('li',{},
+            this.props.text,
+            " Likes: " + this.state.likes,
+            re('button',{onClick: ()=> this.incLikes()}, "Like")
+        )
+    }
+
+    incLikes() {
+        this.setState((state,props)=>({likes: state.likes+1}))
+    }
+}
 
 class ListComponent extends React.Component {
     constructor(props) {
         super(props)
-        this.state=props
+        this.state={...props}
         props.registerItemAdder(item=>this.addItem(item))
     }
 
     render() {
-        return re('ul',{},_.map(this.state.items, i=>re(ListItem,{text:i})))
+        return re('ul',{},
+            _.map(this.state.items, (i,idx)=>re(ListItem,{text:i + ":" + this.state.items.length, key:idx}))
+        )
     }
 
     addItem(item) {
