19a3fc0e343a39cf2c7e7eee0daac5c779805b95
Igorocky <Igorocky@users.noreply.github.com>
2019-09-17 01:52:39 +0200

    Add settings dialog to ImgToCoordsExercise.

--- chess2/js/components/ImgToCoordsExercise.js
+++ chess2/js/components/ImgToCoordsExercise.js
@@ -1,12 +1,21 @@
 'use strict';
 
+function getDirectionTitle(dir) {
+    return dir?"Img->Coords":"Coords->Img"
+}
+
 const ImgToCoordsExercise = ({configName}) => {
-    const [dirImgToCoords, setDirImgToCoords] = useState(true)
-    const [rndElemSelector, setRndElemSelector] = useState(new RandomElemSelector({
-        elems: _.map(ints(0,63), cellNumToCard)
-    }))
+    const [settings, setSettings] = useState({dirImgToCoords:true})
+    const [settingsDialogOpened, setSettingsDialogOpened] = useState(false)
+    const [rndElemSelector, setRndElemSelector] = useState(getNewRndElemSelector())
     const cellSize = "150px"
     const divStyle = {width: cellSize, height: cellSize, fontSize: "120px"}
+
+    function getNewRndElemSelector() {
+        return new RandomElemSelector({
+            elems: _.map(ints(0,63), cellNumToCard)
+        })
+    }
 
     function cellNumToCard(cellNum) {
         return {cellName: getCellName(absNumToCell(cellNum))}
@@ -27,11 +36,80 @@ const ImgToCoordsExercise = ({configName}) => {
         ))
     }
 
+    function renderSettings() {
+        if (!settingsDialogOpened) {
+            return RE.Button({onClick: () => setSettingsDialogOpened(true), color:"primary"},"Settings")
+        } else {
+            return re(ImgToCoordsSettingsDialog,{
+                settings:settings,
+                onSave: newSettings => {
+                    setSettings(newSettings)
+                    setSettingsDialogOpened(false)
+                    setRndElemSelector(getNewRndElemSelector())
+                },
+                onCancel: () => setSettingsDialogOpened(false)
+            })
+        }
+    }
 
-    return re(CardsExercise, {
-        rndElemSelector:rndElemSelector,
-        renderQuestion: card => dirImgToCoords?renderImage(card):renderCoords(card),
-        renderAnswer: card => dirImgToCoords?renderCoords(card):renderImage(card),
-        onIterationComplete: null
-    })
+    function renderExercise() {
+        return re(CardsExercise, {
+            rndElemSelector:rndElemSelector,
+            renderQuestion: card => settings.dirImgToCoords?renderImage(card):renderCoords(card),
+            renderAnswer: card => settings.dirImgToCoords?renderCoords(card):renderImage(card),
+            onIterationComplete: () => setSettingsDialogOpened(true)
+        })
+    }
+
+    function renderExerciseDescription() {
+        return RE.Container.row.center.top({},{},
+            RE.div({style:{fontSize: "30px"}}, getDirectionTitle(settings.dirImgToCoords))
+        )
+    }
+
+    return RE.Container.col.top.center({},{},
+        renderSettings(),
+        renderExerciseDescription(),
+        renderExercise()
+    )
+}
+
+const ImgToCoordsSettingsDialog = ({settings, onSave, onCancel}) => {
+    const [dirImgToCoords, setDirImgToCoords] = useState(settings.dirImgToCoords)
+
+    function getDirValue() {
+        return dirImgToCoords?"ic":"ci"
+    }
+
+    function handleDirChange(event) {
+        setDirImgToCoords("ic" == event.target.value)
+    }
+
+    function renderDirSelector() {
+        return RE.FormControl({component:"fieldset"},
+            RE.FormLabel({component:"legend"},"Direction"),
+            RE.RadioGroup({value:getDirValue(), onChange: handleDirChange, row:true},
+                RE.FormControlLabel({label: "Img->Coords", value: "ic", control: RE.Radio({})}),
+                RE.FormControlLabel({label: "Coords->Img", value: "ci", control: RE.Radio({})}),
+            )
+        )
+    }
+
+    function renderButtons() {
+        return RE.Container.row.left.top({}, {},
+            RE.Button(
+                {onClick: () => onSave({dirImgToCoords: dirImgToCoords}), color: "primary"},
+                "Save"
+            ),
+            RE.Button(
+                {onClick: () => onCancel()},
+                "Cancel"
+            )
+        )
+    }
+
+    return RE.Container.col.top.center({},{},RE.Paper({style:{padding:"10px"}},
+        renderButtons(),
+        renderDirSelector()
+    ))
 }
