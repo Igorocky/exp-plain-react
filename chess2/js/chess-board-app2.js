11991a09b00f800fb11ed4c484cab64e392397a2
Igorocky <Igorocky@users.noreply.github.com>
2019-09-17 23:18:15 +0200

    Optimize TestChessboard component.

--- chess2/js/chess-board-app2.js
+++ chess2/js/chess-board-app2.js
@@ -1,17 +1,12 @@
 'use strict';
 
-const volatile = {}
-
-function handleCellClicked(cell) {
-    apply(volatile.flipCell, cell)
-    apply(volatile.flipImageOnCell, cell)
-}
-
 const TestChessboard = ({configName}) => {
     const {flipCell, flipImageOnCell, renderChessboard} = useChessboard({cellSize:72, configName:configName})
-    volatile.flipCell = flipCell
-    volatile.flipImageOnCell = flipImageOnCell
-    return renderChessboard({onCellClicked:flipImageOnCell})
+    const onCellClicked = useCallback(cell => {
+        flipCell(cell)
+        flipImageOnCell(cell)
+    }, [])
+    return renderChessboard({onCellClicked:onCellClicked})
 }
 
 ReactDOM.render(
