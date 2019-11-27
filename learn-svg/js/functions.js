ad28432205b15f06256cb928286ab89b4d29e023
iieskov <igorieskov5@gmail.com>
2019-11-27 13:42:12 +0100

    Fix learn-svg.html.

--- learn-svg/js/functions.js
+++ learn-svg/js/functions.js
@@ -1,19 +1,34 @@
-const SVG_PARENT_ID = "svg-parent"
-
 var globalIdCounter = 0
 
 function getNextId() {
     return "global-id-" + (globalIdCounter++)
 }
 
-function appendHtmlElem(parentId, htmlElemObj) {
-    const node = document.createElement(htmlElemObj[0])
+function appendSvgElem(htmlElemObj) {
+    const node = document.createElementNS("http://www.w3.org/2000/svg", htmlElemObj[0])
     _.chain(htmlElemObj[1])
         .pairs()
-        .forEach(([k,v]) => node.setAttribute(k,v))
-    document.getElementById(parentId).appendChild(node)
+        .forEach(([k,v]) => {
+            if (k == "style") {
+                node.setAttribute(
+                    "style",
+                    _.chain(v)
+                        .pairs()
+                        .map(([sk,sv]) => sk+":"+sv+";")
+                        .reduce((m,e) => m+e,"")
+                        .value()
+                )
+            } else {
+                node.setAttribute(k,v)
+            }
+        })
+    const id = getNextId()
+    node.setAttribute("id",id)
+    document.getElementById("svg-parent").appendChild(node)
+    return id
 }
 
-function line() {
-    appendHtmlElem("svg-elem", ["line", {x1:0,y1:0,x2:100,y2:100, style:"stroke:#006600;"}])
+function line(x1,y1,x2,y2,attrs) {
+    appendSvgElem(["line", {x1:x1, y1:y1, x2:x2, y2:y2, style:{stroke:"#000000"}, ...attrs}])
 }
+
--- learn-svg/program1.js
+++ learn-svg/program1.js
@@ -1,1 +1,2 @@
-line()
+line(30,70,50,-10)
+line(-30,70,50,-10, {style:{stroke:"red"}})
