15cc1c40db5029310a199b0d0e7e687962697a0b
iieskov <igorieskov5@gmail.com>
2019-11-27 16:31:15 +0100

    Add rect and circle to learn-svg.html.

--- learn-svg/js/functions.js
+++ learn-svg/js/functions.js
@@ -29,6 +29,14 @@ function appendSvgElem(name, attrs) {
 }
 
 function line(x1,y1,x2,y2,attrs) {
-    appendSvgElem("line", {x1:x1, y1:y1, x2:x2, y2:y2, style:{stroke:"#000000"}, ...attrs})
+    appendSvgElem("line", {x1:x1, y1:y1, x2:x2, y2:y2, style:{stroke:"green"}, ...attrs})
+}
+
+function rect(x,y,width,height,attrs) {
+    appendSvgElem("rect", {x:x, y:y, width:width, height:height, style:{stroke:"green", fill:"green"}, ...attrs})
+}
+
+function circle(cx,cy,r,attrs) {
+    appendSvgElem("circle", {cx:cx, cy:cy, r:r, style:{stroke:"green", fill:"green"}, ...attrs})
 }
 
