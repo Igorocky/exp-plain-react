e3720260bfa99f9a3103df1709acbeb546c8a3bc
Igorocky <Igorocky@users.noreply.github.com>
2019-12-27 18:53:50 +0100

    Split cells into groups in KnightMovesExercise.

--- chess/chess-board-app.js
+++ chess/chess-board-app.js
@@ -904,13 +904,33 @@ const KNIGHT_MOVES_PHASE_BOARD_SHOWN = "KNIGHT_MOVES_PHASE_BOARD_SHOWN"
 class KnightMovesExercise extends React.Component {
     constructor(props) {
         super(props)
+
+        const cellsInRandomOrder = [{"x":4,"y":2},{"x":0,"y":3},{"x":6,"y":1},{"x":1,"y":0},{"x":4,"y":0},{"x":6,"y":0},{"x":0,"y":5},{"x":3,"y":3},{"x":4,"y":1},{"x":7,"y":4},{"x":5,"y":2},{"x":2,"y":5},{"x":1,"y":2},{"x":2,"y":4},{"x":6,"y":7},{"x":1,"y":5},{"x":0,"y":2},{"x":2,"y":2},{"x":5,"y":6},{"x":3,"y":5},{"x":7,"y":7},{"x":5,"y":5},{"x":4,"y":3},{"x":3,"y":4},{"x":5,"y":3},{"x":5,"y":4},{"x":2,"y":0},{"x":4,"y":7},{"x":7,"y":5},{"x":4,"y":4},{"x":5,"y":7},{"x":3,"y":0},{"x":3,"y":7},{"x":7,"y":6},{"x":0,"y":1},{"x":6,"y":5},{"x":1,"y":4},{"x":6,"y":6},{"x":2,"y":1},{"x":4,"y":5},{"x":2,"y":7},{"x":7,"y":3},{"x":3,"y":1},{"x":6,"y":2},{"x":0,"y":7},{"x":2,"y":6},{"x":7,"y":0},{"x":0,"y":4},{"x":1,"y":6},{"x":7,"y":1},{"x":4,"y":6},{"x":2,"y":3},{"x":6,"y":4},{"x":1,"y":7},{"x":1,"y":1},{"x":3,"y":2},{"x":5,"y":0},{"x":0,"y":6},{"x":3,"y":6},{"x":5,"y":1},{"x":7,"y":2},{"x":1,"y":3},{"x":6,"y":3},{"x":0,"y":0}]
+        function arrFromTo(arr, from, to) {
+            const res = []
+            ints(from-1,to-1).forEach(i => res.push(arr[i]))
+            return res
+        }
+
         this.state={
             randomElemSelector: new RandomElemSelector({
-                elemsGenerator: listOfAllCellsGenerator
+                // elemsGenerator: listOfAllCellsGenerator
+                elemsGenerator: () => arrFromTo(cellsInRandomOrder, 1, 4)
             }),
             phase: KNIGHT_MOVES_PHASE_IMAGE_SHOWN
         }
         this.handleKeyDownListener = e => this.handleKeyDown(e)
+
+        // const res = new RandomElemSelector({
+        //     elemsGenerator: listOfAllCellsGenerator
+        // })
+        // const cellsInRandomOrder = []
+        // cellsInRandomOrder.push(res.getCurrentElem())
+        // do {
+        //     res.updateStateToNextElem()
+        //     cellsInRandomOrder.push(res.getCurrentElem())
+        // } while (res.getRemainingElements() > 0)
+        // console.log("cellsInRandomOrder = " + JSON.stringify(cellsInRandomOrder));
     }
 
     render() {
