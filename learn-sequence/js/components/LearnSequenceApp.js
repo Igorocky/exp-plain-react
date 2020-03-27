0a14f29b03dd4ee9fd7bfd7c3c24c45ad337c6b8
Igorocky <Igorocky@users.noreply.github.com>
2020-03-27 22:14:47 +0100

    Use single state object approach for LearnSequenceApp.js.

--- learn-sequence/js/components/LearnSequenceApp.js
+++ learn-sequence/js/components/LearnSequenceApp.js
@@ -1,13 +1,22 @@
 'use strict';
 
 const LearnSequenceApp = () => {
-    const [elemsToLearn, setElemsToLearn] = useState(() => PI_DIGITS.split('').map(e => ({value:parseInt(e)})))
-    const [focusedElemIdx, setFocusedElemIdx] = useState(0)
+    const ELEMS_TO_LEARN = "ELEMS_TO_LEARN"
+    const FOCUSED_ELEM_IDX = "FOCUSED_ELEM_IDX"
+
+    const [state, setState] = useState(() => createState())
+
+    function createState() {
+        return {
+            [ELEMS_TO_LEARN]: PI_DIGITS.split('').map(e => ({value:parseInt(e)})),
+            [FOCUSED_ELEM_IDX]: 0
+        }
+    }
 
     useEffect(() => {
         document.addEventListener(KEYDOWN_LISTENER_NAME, onKeyDown)
         return () => document.removeEventListener(KEYDOWN_LISTENER_NAME, onKeyDown)
-    }, [focusedElemIdx])
+    }, [state])
 
     function calcUserInputDigit(keyCode) {
         if (48 <= keyCode && keyCode <= 57) {
@@ -22,51 +31,50 @@ const LearnSequenceApp = () => {
     function onKeyDown(event) {
         const userInputDigit = calcUserInputDigit(event.keyCode)
         if (hasValue(userInputDigit)) {
-            onDigitEntered(userInputDigit)
+            setState(old => onDigitEntered(old, userInputDigit))
         }
     }
 
-    function onDigitEntered(userInputDigit) {
-        setElemsToLearn(oldElemsToLearn => {
-            return oldElemsToLearn.map((e, i) => {
-                if (focusedElemIdx == i) {
-                    if (e.value == userInputDigit) {
-                        setFocusedElemIdx(focusedElemIdx + 1)
-                        return {...e, opened: true}
-                    } else {
-                        return {...e, failed: true}
-                    }
-                } else {
-                    return e
-                }
-            })
-        })
+    function onDigitEntered(state, userInputDigit) {
+        const focusedElemIdx = state[FOCUSED_ELEM_IDX]
+        const elemsToLearn = state[ELEMS_TO_LEARN]
+        const currElem = elemsToLearn[focusedElemIdx]
+        if (currElem.value == userInputDigit) {
+            state = set(state, FOCUSED_ELEM_IDX, focusedElemIdx+1)
+            state = set(state, ELEMS_TO_LEARN, modify(elemsToLearn, focusedElemIdx, e => ({...e, opened: true})))
+        } else {
+            state = set(state, ELEMS_TO_LEARN, modify(elemsToLearn, focusedElemIdx, e => ({...e, failed: true})))
+        }
+        return state
     }
 
-    function openCloseElemsInRow(startIdxClicked) {
-        setElemsToLearn(oldElemsToLearn => {
-            const start = startIdxClicked
-            const end = startIdxClicked+9
-            const elemsInRow = ints(start, end).map(i => oldElemsToLearn[i])
-            const atLeastOneElemIsOpened = elemsInRow.reduce((a,e) => a || e.opened, false)
-            return oldElemsToLearn.map((e,i) => {
+
+    function openCloseElemsInRow(state, startIdxClicked) {
+        const start = startIdxClicked
+        const end = startIdxClicked+9
+        const elemsToLearn = state[ELEMS_TO_LEARN]
+        const elemsInRow = ints(start, end).map(i => elemsToLearn[i])
+        const atLeastOneElemIsOpened = elemsInRow.find(e => e.opened)
+        state = set(state, ELEMS_TO_LEARN,
+            elemsToLearn.map((e,i) => {
                 if (start <= i && i <= end) {
                     return {...e, opened:!atLeastOneElemIsOpened, failed:false}
                 } else {
                     return e
                 }
             })
-        })
+        )
+        return state
     }
 
     return RE.Container.row.left.top({},{style:{margin:"30px"}},
         ints(0,1).map(numberOfHundreds => re(TableOfElems, {
             numberOfHundreds:numberOfHundreds,
-            elems: elemsToLearn,
-            focusedElemIdx:focusedElemIdx,
-            onElemLeftClicked: idxClicked => setFocusedElemIdx(idxClicked),
-            onRowLeftClicked: openCloseElemsInRow,
-            onDigitLeftClicked: onDigitEntered
+            elems: state[ELEMS_TO_LEARN],
+            focusedElemIdx: state[FOCUSED_ELEM_IDX],
+            onElemLeftClicked: idxClicked => setState(old => set(old, FOCUSED_ELEM_IDX, idxClicked)),
+            onRowLeftClicked: startIdxClicked => setState(old => openCloseElemsInRow(old, startIdxClicked)),
+            onDigitLeftClicked: userInputDigit => setState(old => onDigitEntered(old, userInputDigit)),
         }))
     )
 }
--- learn-sequence/js/functions.js
+++ learn-sequence/js/functions.js
@@ -21,60 +21,12 @@ function ints(start, end) {
     return res
 }
 
-function flatMap(list, func) {
-    const res = []
-    _.each(list, elem=>res.push(...func(elem)))
-    return res
+function modify(arr, idx, modifier) {
+    return [...arr.slice(0,idx), modifier(arr[idx]), ...(idx >= arr.length-1 ? [] : arr.slice(idx+1,arr.length))]
 }
 
-function product(...lists) {
-    if (_.size(lists) == 0) return [[]]
-    return flatMap(_.first(lists), elem=>_.map(product(..._.rest(lists)), rightPart=>[elem,...rightPart]))
+function set(obj, attrName, newValue) {
+    return {...obj, [attrName]:newValue}
 }
 
-function clone(obj, modifiers) {
-    return {...(obj?obj:{}), ...(modifiers?modifiers:{})}
-}
-
-function apply(func, ...args) {
-    if (func) {
-        return func(...args)
-    }
-}
-
-class RandomElemSelector {
-    constructor({elems}) {
-        this.origElems = elems
-        this.reset()
-    }
-
-    getCurrentElem() {
-        return this.state.currentElem
-    }
-
-    getIterationNumber() {
-        return this.state.iterationNumber
-    }
-
-    getRemainingElements() {
-        return _.size(this.state.elemsToAsk)
-    }
-
-    reset() {
-        this.state = {elemsToAsk: [], iterationNumber:0}
-        this.loadNextElem()
-    }
-
-    loadNextElem() {
-        let elemsToAsk = this.state.elemsToAsk
-        if (_.size(elemsToAsk)==0) {
-            elemsToAsk = [...this.origElems]
-            this.state.iterationNumber += 1
-        }
-        elemsToAsk = _.shuffle(elemsToAsk)
-        this.state.currentElem = _.first(elemsToAsk)
-        this.state.elemsToAsk = _.rest(elemsToAsk)
-    }
-}
-
-const PI_DIGITS = "3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643"
+const PI_DIGITS = "141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643"
