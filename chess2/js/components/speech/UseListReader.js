138b7f72957c19b71ab28611745f0e4bff921e9e
Igorocky <Igorocky@users.noreply.github.com>
2020-05-23 15:11:55 +0200

    Add sounds to UseListReader.js.

--- chess2/js/components/speech/UseListReader.js
+++ chess2/js/components/speech/UseListReader.js
@@ -8,6 +8,19 @@ function useListReader() {
     const [elems, setElems] = useState([])
     const [currElemIdx, setCurrElemIdx] = useState(0)
 
+    const NEXT_SOUND = "./sounds/on-next.mp3"
+    const PREV_SOUND = "./sounds/on-prev.mp3"
+    const GO_TO_START_SOUND = "./sounds/on-go-to-start3.mp3"
+    const GO_TO_END_SOUND = "./sounds/on-go-to-end-teleport.mp3"
+    const ENTER_SOUND = "./sounds/on-enter2.mp3"
+    const BACKSPACE_SOUND = "./sounds/on-backspace.mp3"
+    const ESCAPE_SOUND = "./sounds/on-escape.mp3"
+
+    function withSound(audioFileName, callback) {
+        const audio = new Audio(audioFileName);
+        audio.play().then(callback)
+    }
+
     function init({say:sayParam, title, elems, sayFirstElem}) {
         if (sayParam !== undefined) {
             setSay(() => nullSafeSay(sayParam))
@@ -50,30 +63,38 @@ function useListReader() {
         }
         const last = symbols[symbols.length-1]
         if (last.sym == MORSE.e.sym) {//ok
-            if (elems.length-1 <= currElemIdx) {
-                say("No more elements to read to the right.")
-            } else {
-                const newCurrElemIdx = currElemIdx+1
-                setCurrElemIdx(newCurrElemIdx)
-                sayElem(newCurrElemIdx)
-            }
+            withSound(NEXT_SOUND, () => {
+                if (elems.length-1 <= currElemIdx) {
+                    say("No more elements to read to the right.")
+                } else {
+                    const newCurrElemIdx = currElemIdx+1
+                    setCurrElemIdx(newCurrElemIdx)
+                    sayElem(newCurrElemIdx)
+                }
+            })
         } else if (last.sym == MORSE.i.sym) {//ok
-            if (currElemIdx <= 0) {
-                say("No more elements to read to the left.")
-            } else {
-                const newCurrElemIdx = currElemIdx-1
-                setCurrElemIdx(newCurrElemIdx)
-                sayElem(newCurrElemIdx)
-            }
+            withSound(PREV_SOUND, () => {
+                if (currElemIdx <= 0) {
+                    say("No more elements to read to the left.")
+                } else {
+                    const newCurrElemIdx = currElemIdx - 1
+                    setCurrElemIdx(newCurrElemIdx)
+                    sayElem(newCurrElemIdx)
+                }
+            })
         } else if (last.sym == MORSE.t.sym) {//ok
             sayElem(currElemIdx)
         } else if (last.sym == MORSE.o.sym) {//ok
-            setCurrElemIdx(0)
-            sayElem(0)
+            withSound(GO_TO_START_SOUND, () => {
+                setCurrElemIdx(0)
+                sayElem(0)
+            })
         } else if (last.sym == MORSE.j.sym) {//ok
-            const lastElemIdx = elems.length-1
-            setCurrElemIdx(lastElemIdx)
-            sayElem(lastElemIdx)
+            withSound(GO_TO_END_SOUND, () => {
+                const lastElemIdx = elems.length-1
+                setCurrElemIdx(lastElemIdx)
+                sayElem(lastElemIdx)
+            })
         } else if (last.sym == MORSE.a.sym) {//ok
             if (title && title.say) {
                 title.say()
@@ -87,13 +108,13 @@ function useListReader() {
                 say("Title spell is not defined.")
             }
         } else if (last.sym == MORSE.m.sym) {//ok
-            onAction(currElemIdx, "onEnter", () => say("On enter is undefined."))
+            withSound(ENTER_SOUND, () => onAction(currElemIdx, "onEnter", () => say("On enter is undefined.")))
         } else if (last.sym == MORSE.n.sym) {//ok
             onAction(currElemIdx, "onSpell", () => say("On spell is undefined."))
         } else if (last.sym == MORSE.s.sym) {//ok
-            onAction(currElemIdx, "onBack", () => say("On back is undefined."))
+            withSound(BACKSPACE_SOUND, () => onAction(currElemIdx, "onBack", () => say("On back is undefined.")))
         } else if (last.sym == MORSE.error.sym) {//ok
-            onAction(currElemIdx, "onEscape", () => say("On escape is undefined."))
+            withSound(ESCAPE_SOUND, () => onAction(currElemIdx, "onEscape", () => say("On escape is undefined.")))
         } else {
             say("Unexpected command: " + last.codeInfo.word)
         }
