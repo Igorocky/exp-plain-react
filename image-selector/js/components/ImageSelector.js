65e3aab2fca28ab506af0302a4c19f321efaf72c
Igorocky <Igorocky@users.noreply.github.com>
2021-05-16 10:46:48 +0200

    Improve performance of function normalize(b1, b2).

--- image-selector/js/components/ImageSelector.js
+++ image-selector/js/components/ImageSelector.js
@@ -104,7 +104,11 @@ const ImageSelector = () => {
         } else if (b2.getPoints().every(p => b1.includesPoint(p))) {
             return [b1]
         } else {
-            const result = []
+            const resultF = [
+                [false,false,false],
+                [false,false,false],
+                [false,false,false],
+            ]
             const xs = [b1.minX,b2.minX,b1.maxX,b2.maxX]
             xs.sort()
             const ys = [b1.minY,b2.minY,b1.maxY,b2.maxY]
@@ -117,7 +121,40 @@ const ImageSelector = () => {
                     )
                     const mid = new Point((b.minX+b.maxX)/2,(b.minY+b.maxY)/2)
                     if (b1.includesPoint(mid) || b2.includesPoint(mid)) {
-                        result.push(b)
+                        resultF[xi][yi] = true
+                    }
+                }
+            }
+            const result = []
+            for (let yi = 0; yi < 3; yi++) {
+                if (resultF[0][yi] && resultF[1][yi] && resultF[2][yi]) {
+                    result.push(SvgBoundaries.fromPoints(
+                        new Point(xs[0],ys[yi]),
+                        new Point(xs[3],ys[yi+1]),
+                    ))
+                    resultF[0][yi] = false
+                    resultF[1][yi] = false
+                    resultF[2][yi] = false
+                }
+            }
+            for (let xi = 0; xi < 3; xi++) {
+                if (resultF[xi][0] && resultF[xi][1] && resultF[xi][2]) {
+                    result.push(SvgBoundaries.fromPoints(
+                        new Point(xs[xi],ys[0]),
+                        new Point(xs[xi+1],ys[3]),
+                    ))
+                    resultF[xi][0] = false
+                    resultF[xi][1] = false
+                    resultF[xi][2] = false
+                }
+            }
+            for (let xi = 0; xi < 3; xi++) {
+                for (let yi = 0; yi < 3; yi++) {
+                    if (resultF[xi][yi]) {
+                        result.push(SvgBoundaries.fromPoints(
+                            new Point(xs[xi],ys[yi]),
+                            new Point(xs[xi+1],ys[yi+1]),
+                        ))
                     }
                 }
             }
