37a2d3577279abf095addd83fefc5a9b3c5e5891
igorocky <123@gmail.com>
2021-09-14 23:26:05 +0200

    AudioCellsExercise: connect a gamepad.

--- chess3/js/components/AudioCellsExercise.js
+++ chess3/js/components/AudioCellsExercise.js
@@ -1,5 +1,20 @@
 "use strict";
 
+let GAMEPAD_BUTTONS = null
+let GAMEPAD_AXES = null
+let GAMEPAD_STATE_CHANGE_LISTENER = null
+const GAMEPAD_BUTTON_X = 3
+const GAMEPAD_BUTTON_Y = 4
+const GAMEPAD_BUTTON_A = 0
+const GAMEPAD_BUTTON_B = 1
+const GAMEPAD_BUTTON_SELECT = 10
+const GAMEPAD_BUTTON_START = 11
+const GAMEPAD_BUTTON_LEFT = 100
+const GAMEPAD_BUTTON_RIGHT = 101
+const GAMEPAD_BUTTON_UP = 102
+const GAMEPAD_BUTTON_DOWN = 103
+const GAMEPAD_BUTTON_LEFT_SHIFT = 6
+const GAMEPAD_BUTTON_RIGHT_SHIFT = 7
 const AudioCellsExercise = () => {
     const s = {
         PHASE: 'PHASE',
@@ -36,12 +51,13 @@ const AudioCellsExercise = () => {
     }
 
     function onUserInput(symOrCode) {
+        const allowedButtonPressedSound = 'on-next.mp3'
         setState(state => {
             const st = objectHolder(state)
 
             if ('5' === symOrCode) {
-                sayCard(st.get(s.CURR_CARD))
-            } else if ('6' === symOrCode) {
+                playAudio(allowedButtonPressedSound, () => sayCard(st.get(s.CURR_CARD)))
+            } else if ('4' === symOrCode) {
                 const nextCard = nextRandomElem({allElems: st.get(s.ALL_CARDS), counts: st.get(s.CARD_COUNTS)});
                 st.set(s.CURR_CARD, nextCard)
                 st.set(s.CARD_COUNTS, inc(st.get(s.CARD_COUNTS), st.get(s.CURR_CARD).idx))
@@ -53,7 +69,7 @@ const AudioCellsExercise = () => {
             if (hasValue(st.get(s.WAKEUP_TIMEOUT_HANDLE))) {
                 clearTimeout(st.get(s.WAKEUP_TIMEOUT_HANDLE))
             }
-            st.set(s.WAKEUP_TIMEOUT_HANDLE, setTimeout(() => playAudio('on-go-to-start.mp3'), 4.5*60*1000))
+            st.set(s.WAKEUP_TIMEOUT_HANDLE, setTimeout(() => playAudio('on-go-to-start3.mp3'), 3.5*60*1000))
 
             return st.get()
         })
@@ -74,6 +90,88 @@ const AudioCellsExercise = () => {
     function renderCurrCell() {
         const {x,y} = state[s.CURR_CARD]
         return RE.span({style:{fontSize:'30px'}}, xCoordToStr(x).toUpperCase()+yCoordToStr(y))
+    }
+
+    GAMEPAD_STATE_CHANGE_LISTENER = ({button,pressed}) => {
+        if (pressed) {
+            if (button == GAMEPAD_BUTTON_START) {
+                playAudio('on-enter.mp3')
+            } else {
+                onUserInput(
+                    button == GAMEPAD_BUTTON_RIGHT ? '5'
+                        : button == GAMEPAD_BUTTON_UP ? '4'
+                            : '3'
+                )
+            }
+        }
+    }
+
+    function readGamepadState() {
+        const gamepads = navigator.getGamepads()
+        let gamepad = null
+        for (let i = 0; i < gamepads.length; i++) {
+            if (gamepads[i]?.id == 'Bluetooth Wireless Controller (Vendor: 05a0 Product: 3232)') {
+                gamepad = gamepads[i]
+            }
+        }
+        if (!gamepad?.connected) {
+            GAMEPAD_BUTTONS = null
+            GAMEPAD_AXES = null
+            return
+        } else if (!GAMEPAD_BUTTONS) {
+            console.log('connected')
+            GAMEPAD_BUTTONS = gamepad.buttons
+            GAMEPAD_AXES = gamepad.axes
+            return
+        } else {
+            const gamepadButtons = gamepad.buttons
+            let events = []
+            function addEvent({button,pressed}) {
+                events.push({button,pressed})
+            }
+            for (let i = 0; i < gamepadButtons.length; i++) {
+                let buttonNumber = null
+                let pressed = null
+                if (!GAMEPAD_BUTTONS[i].pressed && gamepadButtons[i].pressed) {
+                    buttonNumber = i
+                    pressed = true
+                } else if (GAMEPAD_BUTTONS[i].pressed && !gamepadButtons[i].pressed) {
+                    buttonNumber = i
+                    pressed = false
+                }
+                if (hasValue(buttonNumber)) {
+                    console.log(`Button ${buttonNumber} was ${pressed?'pressed':'released'}.`)
+                    addEvent({button:buttonNumber,pressed})
+                }
+            }
+            GAMEPAD_BUTTONS = gamepad.buttons
+
+            const gamepadAxes = gamepad.axes
+            for (let i = 0; i < gamepadAxes.length; i++) {
+                if (Math.abs(GAMEPAD_AXES[i] - gamepadAxes[i]) > 0.5) {
+                    const value = gamepadAxes[i]
+                    const delta = gamepadAxes[i] - GAMEPAD_AXES[i]
+                    const pressed = Math.abs(value) > 0.5
+                    const buttonNumber = 100 + i*2 + (pressed ? (delta < 0 ? 0 : 1) : (delta > 0 ? 0 : 1))
+                    console.log(`Button ${buttonNumber} was ${pressed?'pressed':'released'}.`)
+                    addEvent({button:buttonNumber,pressed})
+                }
+            }
+            GAMEPAD_AXES = gamepad.axes
+
+            if (GAMEPAD_STATE_CHANGE_LISTENER) {
+                for (let event of events) {
+                    GAMEPAD_STATE_CHANGE_LISTENER(event)
+                }
+            }
+        }
+    }
+
+    function connectGamepad() {
+        window.setInterval(
+            readGamepadState,
+            30
+        )
     }
 
     return RE.Container.col.top.center({style:{marginTop:'0px'}},{style:{marginTop:'15px'}},
@@ -97,5 +195,6 @@ const AudioCellsExercise = () => {
         }),
         renderCurrCell(),
         renderStatistics(),
+        RE.Button({onClick: connectGamepad, style:{color:'white'}}, 'Connect game pad'),
     )
 }
